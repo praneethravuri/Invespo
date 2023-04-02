44de04550ba55b16078fc1b598d2b6b516e054ab
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-02 05:05:15 -0400

    Implemented search box

--- JS/stock-page.js
+++ JS/stock-page.js
@@ -18,60 +18,30 @@ let data = {
         "name" : "Praneeth Ravuri",
         "portfolio_1" : {
             "msft": {
-                "companyName": "Microsoft",
-                "ticker": "MSFT",
                 "quantity": 10,
                 "boughtOn": "01/03/2022",
-                "originalPrice":329.01,
-                "currentPrice": 252.51,
-                "type" : "domestic"
             },
             "aapl": {
-                "companyName": "Apple",
-                "ticker": "AAPL",
                 "quantity": 110,
                 "boughtOn": "09/30/2022",
-                "originalPrice":138.20,
-                "currentPrice": 144.49,
-                "type" : "domestic"
             },
             "ali": {
-                "companyName": "Alibaba Holding Company",
-                "ticker": "ALI",
                 "quantity": 1000,
                 "boughtOn": "01/04/2023",
-                "originalPrice":91.98,
-                "currentPrice": 110.83,
-                "type" : "foreign"
             }
         },
         "portfolio_2" : {
             "nflx": {
-                "companyName": "Netflix",
-                "ticker": "NFLX",
                 "quantity": 20,
                 "boughtOn": "12/15/2022",
-                "originalPrice": 558.10,
-                "currentPrice": 584.50,
-                "type": "domestic"
             },
             "googl": {
-                "companyName": "Alphabet",
-                "ticker": "GOOGL",
                 "quantity": 5,
                 "boughtOn": "08/20/2022",
-                "originalPrice": 2735.73,
-                "currentPrice": 2216.00,
-                "type": "domestic"
             },
             "baba": {
-                "companyName": "Alibaba Group",
-                "ticker": "BABA",
                 "quantity": 50,
                 "boughtOn": "06/02/2022",
-                "originalPrice": 219.78,
-                "currentPrice": 233.40,
-                "type": "foreign"
             }
         }
     },
@@ -79,60 +49,30 @@ let data = {
         "name" : "John Doe",
         "portfolio_1" : {
             "amzn": {
-                "companyName": "Amazon",
-                "ticker": "AMZN",
                 "quantity": 5,
                 "boughtOn": "11/01/2022",
-                "originalPrice": 3155.91,
-                "currentPrice": 3052.03,
-                "type": "domestic"
             },
             "nvda": {
-                "companyName": "Nvidia",
-                "ticker": "NVDA",
                 "quantity": 15,
                 "boughtOn": "07/17/2022",
-                "originalPrice": 208.79,
-                "currentPrice": 195.22,
-                "type": "domestic"
             },
             "tcehy": {
-                "companyName": "Tencent Holdings",
-                "ticker": "TCEHY",
                 "quantity": 200,
                 "boughtOn": "06/05/2022",
-                "originalPrice": 70.41,
-                "currentPrice": 75.66,
-                "type": "foreign"
             }
         },
         "portfolio_2" : {
             "tsla": {
-                "companyName": "Tesla",
-                "ticker": "TSLA",
                 "quantity": 10,
                 "boughtOn": "09/15/2022",
-                "originalPrice": 748.74,
-                "currentPrice": 670.00,
-                "type": "domestic"
             },
             "nflx": {
-                "companyName": "Netflix",
-                "ticker": "NFLX",
                 "quantity": 20,
                 "boughtOn": "12/22/2022",
-                "originalPrice": 573.24,
-                "currentPrice": 584.50,
-                "type": "domestic"
             },
             "baba": {
-                "companyName": "Alibaba Group",
-                "ticker": "BABA",
                 "quantity": 40,
                 "boughtOn": "08/07/2022",
-                "originalPrice": 221.11,
-                "currentPrice": 233.40,
-                "type": "foreign"
             }
         }
     },
@@ -140,64 +80,179 @@ let data = {
         "name" : "XYZ",
         "portfolio_1" : {
             "amzn": {
-                "companyName": "Amazon",
-                "ticker": "AMZN",
                 "quantity": 20,
                 "boughtOn": "07/01/2022",
-                "originalPrice": 3605.94,
-                "currentPrice": 3381.00,
-                "type": "domestic"
             },
             "nke": {
-                "companyName": "Nike",
-                "ticker": "NKE",
                 "quantity": 30,
                 "boughtOn": "05/15/2022",
-                "originalPrice": 152.50,
-                "currentPrice": 165.20,
-                "type": "domestic"
             },
             "baba": {
-                "companyName": "Alibaba Group",
-                "ticker": "BABA",
                 "quantity": 50,
                 "boughtOn": "06/02/2022",
-                "originalPrice": 219.78,
-                "currentPrice": 233.40,
-                "type": "foreign"
             }
         },
         "portfolio_2" : {
             "nvda": {
-                "companyName": "NVIDIA",
-                "ticker": "NVDA",
                 "quantity": 10,
                 "boughtOn": "01/01/2023",
-                "originalPrice": 339.79,
-                "currentPrice": 329.50,
-                "type": "domestic"
             },
             "tsm": {
-                "companyName": "Taiwan Semiconductor Manufacturing Company",
-                "ticker": "TSM",
                 "quantity": 80,
                 "boughtOn": "11/20/2022",
-                "originalPrice": 111.44,
-                "currentPrice": 126.80,
-                "type": "foreign"
             },
             "crm": {
-                "companyName": "Salesforce",
-                "ticker": "CRM",
                 "quantity": 25,
                 "boughtOn": "09/10/2022",
-                "originalPrice": 267.68,
-                "currentPrice": 276.60,
-                "type": "domestic"
             }
         }
     }
 }
+
+
+
+let companies = {
+    "msft": {
+        "companyName": "Microsoft",
+        "originalPrice":329.01,
+        "currentPrice": 252.51,
+        "type" : "domestic"
+    },
+    "aapl": {
+        "companyName": "Apple",
+        "originalPrice":138.20,
+        "currentPrice": 144.49,
+        "type" : "domestic"
+    },
+    "ali": {
+        "companyName": "Alibaba Holding Company",
+        "originalPrice":91.98,
+        "currentPrice": 110.83,
+        "type" : "foreign"
+    },
+    "nflx": {
+        "companyName": "Netflix",
+        "originalPrice": 558.10,
+        "currentPrice": 584.50,
+        "type": "domestic"
+    },
+    "googl": {
+        "companyName": "Alphabet",
+        "originalPrice": 2735.73,
+        "currentPrice": 2216.00,
+        "type": "domestic"
+    },
+    "baba": {
+        "companyName": "Alibaba Group",
+        "originalPrice": 219.78,
+        "currentPrice": 233.40,
+        "type": "foreign"
+    },
+    "amzn": {
+        "companyName": "Amazon",
+        "originalPrice": 3155.91,
+        "currentPrice": 3052.03,
+        "type": "domestic"
+    },
+    "nvda": {
+        "companyName": "Nvidia",
+        "originalPrice": 208.79,
+        "currentPrice": 195.22,
+        "type": "domestic"
+    },
+    "tcehy": {
+        "companyName": "Tencent Holdings",
+        "originalPrice": 70.41,
+        "currentPrice": 75.66,
+        "type": "foreign"
+    },
+    "tsla": {
+        "companyName": "Tesla",
+        "originalPrice": 748.74,
+        "currentPrice": 670.00,
+        "type": "domestic"
+    },
+    "nflx": {
+        "companyName": "Netflix",
+        "originalPrice": 573.24,
+        "currentPrice": 584.50,
+        "type": "domestic"
+    },
+    "baba": {
+        "companyName": "Alibaba Group",
+        "originalPrice": 221.11,
+        "currentPrice": 233.40,
+        "type": "foreign"
+    },
+    "amzn": {
+        "companyName": "Amazon",
+        "originalPrice": 3605.94,
+        "currentPrice": 3381.00,
+        "type": "domestic"
+    },
+    "nke": {
+        "companyName": "Nike",
+        "originalPrice": 152.50,
+        "currentPrice": 165.20,
+        "type": "domestic"
+    },
+    "baba": {
+        "companyName": "Alibaba Group",
+        "originalPrice": 219.78,
+        "currentPrice": 233.40,
+        "type": "foreign"
+    },
+    "nvda": {
+        "companyName": "NVIDIA",
+        "originalPrice": 339.79,
+        "currentPrice": 329.50,
+        "type": "domestic"
+    },
+    "tsm": {
+        "companyName": "Taiwan Semiconductor Manufacturing Company",
+        "originalPrice": 111.44,
+        "currentPrice": 126.80,
+        "type": "foreign"
+    },
+    "crm": {
+        "companyName": "Salesforce",
+        "originalPrice": 267.68,
+        "currentPrice": 276.60,
+        "type": "domestic"
+    }
+}
+
+// Search box for stocks
+$(document).ready(function(){
+    $("#stock-lookup-table").hide();
+    for(let st in companies){
+        let row = "<tr>"
+        row += "<td>" + st.toUpperCase() + "</td>"
+        row += "<td>" + companies[st]["companyName"] + "</td>"
+        row += "<td>" + companies[st]["originalPrice"] + "</td>"
+        row += "<td>" + companies[st]["currentPrice"] + "</td>"
+        row += "<td>" + companies[st]["type"] + "</td>"
+        $("#lookup").append(row);
+    }
+});
+
+$(document).ready(function(){
+    $("#search-input").on("keyup", function(){
+        let value = $(this).val().toLowerCase();
+        $("#lookup tr").filter(function(){
+            $("#stock-lookup-table").show();
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    }).on("keyup", function(){
+        if($(this).val() === '') {
+            $("#stock-lookup-table").hide();
+        }
+    });
+});
+
+
+
+let companiesList = Object.keys(companies);
 
 let p1 = data[email].portfolio_1;
 let p2 = data[email].portfolio_2;
@@ -205,19 +260,17 @@ let p2 = data[email].portfolio_2;
 let p1Stocks = Object.keys(p1);
 let p2Stocks = Object.keys(p2);
 
-// data => email => portfolio => ticker => company properties
 
-function calculatePNL(currComp){
-    let currPNL = currComp["quantity"] * (currComp["currentPrice"] - currComp["originalPrice"]);
+// data => email => portfolio => ticker => company properties
+// companies => ticker => company name => original price => current price => type
+
+function calculatePNL(currCompQuantity, currCompCP, currCompOP){
+    let currPNL = currCompQuantity * (currCompCP - currCompOP);
     return Math.round(currPNL * 100) / 100;
 }
 
-function displayOrderBox(currTicker, currCompName, currCompQuantity, currCompOP, currCompCP){
-    console.log(currCompCP);
-    document.getElementById("stock-name").innerHTML = currCompName + " (" + currTicker + ")";
-    document.getElementById("originalPrice").innerHTML = currCompOP;
-    document.getElementById("currentPrice").innerHTML = currCompCP;
-
+function displayOrderBox(currTicker, currCompName, currCompCP, currCompOP){
+    console.log(currCompName);
 }
 
 $(document).ready(function(){
@@ -229,11 +282,11 @@ $(document).ready(function(){
         for(let i = 0; i< p1Stocks.length; i++){
             let currTicker = p1Stocks[i]; // msft
             let currComp = p1[currTicker]; // {msft: {...}}
-            let currCompType = p1[currTicker]["type"]; // domestic
-            let currCompName = p1[currTicker]["companyName"]; // microsoft
+            let currCompType = companies[currTicker]["type"] // domestic
+            let currCompName = companies[currTicker]["companyName"]; // microsoft
             let currCompQuantity = p1[currTicker]["quantity"]; // quantity
-            let currCompOP = p1[currTicker]["originalPrice"]; // original price
-            let currCompCP = p1[currTicker]["currentPrice"]; // current price
+            let currCompOP = companies[currTicker]["originalPrice"]; // original price
+            let currCompCP = companies[currTicker]["currentPrice"]; // current price
             let currCompDate = p1[currTicker]["boughtOn"]; // date
             if(currCompType === "domestic"){
                 let row = "<tr>"
@@ -243,21 +296,18 @@ $(document).ready(function(){
                 row += "<td>" + currCompDate + "</td>"
                 row += "<td>" + currCompOP + "</td>"
                 row += "<td>" + currCompCP + "</td>"
-                row += "<td>" + calculatePNL(currComp)  + "</td>"
-                row += "<td> <button class='order-btn' onclick='displayOrderBox(\"" + currTicker + "\", \"" + currCompName + "\", \"" + currCompQuantity + "\", \"" + currCompOP + "\", \"" + currCompCP + "\")'>Order</button> </td>"
+                row += "<td>" + calculatePNL(currCompQuantity, currCompCP, currCompOP)  + "</td>"
                 $(".domestic-stocks").append(row);
             }
             else if(currCompType === "foreign"){
                 let row = "<tr>"
-                row += "<td>" + currComp["companyName"] + "</td>"
-                row += "<td>" + currComp["ticker"] + "</td>"
-                row += "<td>" + currComp["quantity"] + "</td>"
-                row += "<td>" + currComp["boughtOn"] + "</td>"
-                row += "<td>" + currComp["originalPrice"] + "</td>"
-                row += "<td>" + currComp["currentPrice"] + "</td>"
-                row += "<td>" + calculatePNL(currComp)  + "</td>"
-                row += "<td>" + "trade" + "</td>"
-                row += "</tr>";
+                row += "<td>" + currCompName + "</td>"
+                row += "<td>" + currTicker + "</td>"
+                row += "<td>" + currCompQuantity + "</td>"
+                row += "<td>" + currCompDate + "</td>"
+                row += "<td>" + currCompOP + "</td>"
+                row += "<td>" + currCompCP + "</td>"
+                row += "<td>" + calculatePNL(currCompQuantity, currCompCP, currCompOP)  + "</td>"
                 $(".foreign-stocks").append(row);
             }
         }
@@ -271,36 +321,36 @@ $(document).ready(function(){
         $(".domestic-stocks").show();
         $(".foreign-stocks").show();
         for(let i = 0; i< p2Stocks.length; i++){
-            let currTicker = p2Stocks[i];
-            let currComp = p2[currTicker];
-            let currCompType = p2[currTicker]["type"];
+            let currTicker = p2Stocks[i]; // msft
+            let currComp = p2[currTicker]; // {msft: {...}}
+            let currCompType = companies[currTicker]["type"] // domestic
+            let currCompName = companies[currTicker]["companyName"]; // microsoft
+            let currCompQuantity = p2[currTicker]["quantity"]; // quantity
+            let currCompOP = companies[currTicker]["originalPrice"]; // original price
+            let currCompCP = companies[currTicker]["currentPrice"]; // current price
+            let currCompDate = p2[currTicker]["boughtOn"]; // date
             if(currCompType === "domestic"){
                 let row = "<tr>"
-                row += "<td>" + currComp["companyName"] + "</td>"
-                row += "<td>" + currComp["ticker"] + "</td>"
-                row += "<td>" + currComp["quantity"] + "</td>"
-                row += "<td>" + currComp["boughtOn"] + "</td>"
-                row += "<td>" + currComp["originalPrice"] + "</td>"
-                row += "<td>" + currComp["currentPrice"] + "</td>"
-                row += "<td>" + calculatePNL(currComp)  + "</td>"
-                row += "<td>" + "trade" + "</td>"
-                row += "</tr>";
+                row += "<td>" + currCompName + "</td>"
+                row += "<td>" + currTicker + "</td>"
+                row += "<td>" + currCompQuantity + "</td>"
+                row += "<td>" + currCompDate + "</td>"
+                row += "<td>" + currCompOP + "</td>"
+                row += "<td>" + currCompCP + "</td>"
+                row += "<td>" + calculatePNL(currCompQuantity, currCompCP, currCompOP)  + "</td>"
                 $(".domestic-stocks").append(row);
             }
             else if(currCompType === "foreign"){
                 let row = "<tr>"
-                row += "<td>" + currComp["companyName"] + "</td>"
-                row += "<td>" + currComp["ticker"] + "</td>"
-                row += "<td>" + currComp["quantity"] + "</td>"
-                row += "<td>" + currComp["boughtOn"] + "</td>"
-                row += "<td>" + currComp["originalPrice"] + "</td>"
-                row += "<td>" + currComp["currentPrice"] + "</td>"
-                row += "<td>" + calculatePNL(currComp)  + "</td>"
-                row += "<td>" + "trade" + "</td>"
-                row += "</tr>";
+                row += "<td>" + currCompName + "</td>"
+                row += "<td>" + currTicker + "</td>"
+                row += "<td>" + currCompQuantity + "</td>"
+                row += "<td>" + currCompDate + "</td>"
+                row += "<td>" + currCompOP + "</td>"
+                row += "<td>" + currCompCP + "</td>"
+                row += "<td>" + calculatePNL(currCompQuantity, currCompCP, currCompOP)  + "</td>"
                 $(".foreign-stocks").append(row);
             }
         }
     });
 });
-
